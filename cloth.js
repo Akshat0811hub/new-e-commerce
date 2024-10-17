44697fc41bb4f932382f7dd79cc8ed58315c4ab6
Akshat Singh <singhakshat875@gmail.com>
2024-10-17 15:00:18 +0530

    done

---
+++ cloth.js
@@ -0,0 +1,26 @@
+fetch('https://fakestoreapi.com/products')
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('Network response was not ok ' + res.statusText);
+            }
+            return res.json();
+        })
+        .then(json => {
+            const productList = document.getElementById('product-list');
+            json.forEach(product => {
+                console.log(json);
+                const productDiv = document.createElement('div');
+                productDiv.className = 'product';
+                productDiv.innerHTML = `<a><img src="${product.image}" alt="${product.title}">
+                    <div>
+                        <h2>${product.title}</h2>
+                      
+                        <p><strong>Price: $${product.price}</strong></p>
+                        <p>Rate : ${product.rating.rate}</p></a>
+                    </div>
+                  
+                `;
+                productList.appendChild(productDiv);
+            });
+        })
+        .catch(err => console.error('Error fetching products:', err));
